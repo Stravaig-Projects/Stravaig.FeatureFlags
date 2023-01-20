afe867f033f6161d433d848fa0dc95cfee3ec886
Colin Mackay <github@colinmackay.scot>
2023-01-20 14:30:54 +0000

    Add main project & test project

---
+++ src/Stravaig.FeatureFlags/IStronglyTypedFeatureFlag.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading.Tasks;
+
+namespace Stravaig.FeatureFlags;
+
+public interface IStronglyTypedFeatureFlag
+{
+    /// <summary>
+    /// Gets whether the feature is active or not.
+    /// </summary>
+    /// <returns>True if the feature is active; false otherwise.</returns>
+    Task<bool> IsEnabledAsync();
+
+    /// <summary>
+    /// Gets whether the feature is active or not marshalling the result to a synchronous context. This is slow, use the Async variant if possible.
+    /// </summary>
+    /// <returns>True if the feature is active; false otherwise.</returns>
+    bool IsEnabled();
+}
