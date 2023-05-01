6d25837b621748fc7582edb56b9f284158b5ca08
Colin Mackay <github@colinmackay.scot>
2023-05-01 22:14:49 +0100

    Generate the namespace part of the file

---
+++ src/Stravaig.FeatureFlags.Tests/NamespaceTests.cs
@@ -0,0 +1,44 @@
+namespace Stravaig.FeatureFlags.Tests;
+
+[TestFixture]
+public class NamespaceTests : VerifySourceGeneratorTests
+{
+    [Test]
+    public async Task FileScopedNamespaceTest()
+    {
+        const string source = @"
+using Stravaig.FeatureFlags;
+
+namespace My.Test.Namespace;
+
+[StronglyTypedFeatureFlags]
+public enum FeatureFlags
+{
+    FeatureOne,
+    FeatureTwo,
+}
+";
+
+        await VerifyGeneratedSource(source);
+    }
+    
+    [Test]
+    public async Task BlockScopedNamespaceTest()
+    {
+        const string source = @"
+using Stravaig.FeatureFlags;
+
+namespace My.Test.Namespace
+{
+    [StronglyTypedFeatureFlags]
+    public enum FeatureFlags
+    {
+        FeatureOne,
+        FeatureTwo,
+    }
+}
+";
+
+        await VerifyGeneratedSource(source);
+    }
+}
